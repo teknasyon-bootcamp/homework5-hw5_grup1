be663850e895e0fccce90e71e653f46074b62b43
S. Utku Olgun <smtkuo@gmail.com>
2021-09-16 01:08:24 +0300

    mongodb sınıfı güncellemesi

--- class/database/engine/mongodb.class.php
+++ class/database/engine/mongodb.class.php
@@ -1,37 +1,82 @@
 <?php
-
-namespace class/database/engine;
-
-class MongoDB implements DriverInterface
+class MongoDB implements Driver
 {
-    protected MongoDB\Client $client; 
-
-    public function __construct(string $protocol, string $host, string $user, string $pass, string $dbname, array $options)
-    {
+    public $db;
+	private $dbname;
+	private $tablename;
+    public function __construct(String $dbname,String $tablename)
+	{
+		$db = new MongoDB\Driver\Manager('mongodb://mongo');
+		$this->db = $db;
+		$this->dbname = $dbname;
+		$this->tablename = $tablename;
     }
-
-    public function all(string $table): array
+	public function select(String $dbname,String $tablename):void
+	{
+		$this->dbname = $dbname;
+		$this->tablename = $tablename;
+    } 
+    public function all():array
     {
-        // TODO: Implement all() method.
+		$table = $this->dbname.".".$this->tablename;
+		$sorgu = new MongoDB\Driver\Query([]);
+		$sonuc = $this->db->executeQuery($table, $sorgu)->toArray();
+        return $sonuc;
     }
-
-    public function find(string $table, mixed $id): mixed
+    public function find(Array $select,Array $options):mixed
     {
-        // TODO: Implement find() method.
+		$table = $this->dbname.".".$this->tablename;
+		$query = new MongoDB\Driver\Query($select,$options);
+		$sonuc = $this->db->executeQuery($table, $query)->toArray();
+        return $sonuc; 
+	}
+    public function create(Array $data):void
+    {
+		$table = $this->dbname.".".$this->tablename;  
+		$write = new MongoDB\Driver\BulkWrite();
+		$write->insert($data);
+		$this->db->executeBulkWrite($table,$write);
     }
-
-    public function create(string $table, array $values): bool
+	public function update(Array $select,Array $newData):bool
     {
-        // TODO: Implement create() method.
+		$table = $this->dbname.".".$this->tablename; 
+		$veri = new MongoDB\Driver\BulkWrite();
+		$veri->update($select, ['$set' => $newData]);
+		$sonuc = $this->db->executeBulkWrite($table, $veri);
+		return $sonuc->getModifiedCount();
     }
-
-    public function update(string $table, mixed $id, array $values): bool
+	public function delete(Array $select):bool
     {
-        // TODO: Implement update() method.
-    }
-
-    public function delete(string $table, mixed $id): bool
-    {
-        // TODO: Implement delete() method.
+		$table = $this->dbname.".".$this->tablename; 
+		$veri = new MongoDB\Driver\BulkWrite();
+		$veri->delete($select);
+		$sonuc = $this->db->executeBulkWrite($table, $veri);
+		return $sonuc->getDeletedCount();
     }
 }
+/* 
+Örnekler 
+// Bağlantı Kurma
+$Mongo = new MongoDB("default","book");
+// Veri Ekleme
+$Mongo->create(array(
+"id"=>$Mongo->db->counters,
+"name"=>"İğrenç bir hayat",
+"summary"=>"Gerçekten",
+"image_url"=>"./image.png",
+"created_at"=>time(),
+"update_at"=>time()
+)); 
+// Veri Gösterme
+$Result = $Mongo->all();
+print_r($Result);
+// Veri Bulma İşlemi
+$Result = $Mongo->find(["created_at"=>1631741737],[]);
+print_r($Result); 
+// Güncelleme İşlemi
+$Result = $Mongo->update(["created_at"=>1631741737],["name"=>"Güzel bir Hayat"]);
+print_r($Result); 
+// Silme İşlemi
+$Result = $Mongo->delete(["created_at"=>1631741737]);
+print_r($Result); 
+*/
