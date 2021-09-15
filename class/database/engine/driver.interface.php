f398caae0e510c6b5c477cf42714c06b32dbb8de
Hasan UYANIK <40677383+hasanuyanik@users.noreply.github.com>
2021-09-15 10:29:45 +0300

    Mysql'in ilk kısım prototip kodlanması

--- class/database/engine/driver.interface.php
+++ class/database/engine/driver.interface.php
@@ -2,8 +2,8 @@
 interface Driver
 {
     public function all(String $table):array;
-    public function find(String $table,$id):mixed;
-    public function create(String $table,$values):bool;
-    public function update(String $table,$id,$values):bool;
-    public function delete(String $table,$id):bool;
+    public function find(String $table,mixed $id):mixed;
+    public function create(String $table,array $values):bool;
+    public function update(String $table,mixed $id,array $values):bool;
+    public function delete(String $table,mixed $id):bool;
 }
--- class/database/engine/mysql.class.php
+++ class/database/engine/mysql.class.php
@@ -1,1 +1,57 @@
 <?php
+
+class mysql extends PDO
+{
+    private PDO $PDO;
+    public function __construct(
+        String $host,
+        String $user,
+        String $pass,
+        String $dbname
+    ){
+
+        $this->PDO = new PDO("mysql:host=$host;dbname=$dbname",$user,$pass);
+
+    }
+
+    public function all(String $table):array
+    {
+        $query = "SELECT * FROM $table";
+
+        $statement = $this->PDO->prepare($query);
+
+        $statement->execute();
+
+        $result = $statement->fetchAll();
+
+        return $result;
+    }
+
+    public function find(String $table,mixed $id):mixed
+    {
+        $idValue = (is_numeric($id) || is_string($id)) ? $id : $id['id'];
+
+        $query = "SELECT * FROM $table WHERE id=:id";
+
+        $statement = $this->PDO->prepare($query);
+
+        $statement->execute([
+            'id' => $idValue
+        ]);
+
+        $result = $statement->fetch();
+
+        return $result;
+    }
+
+    public function create(String $table,array $values):bool
+    {
+        $query = "INSERT INTO $table() values()";
+
+        $statement = $this->PDO->prepare($query);
+
+        $result = $statement->execute($valueArray);
+
+        return $result;
+    }
+}
