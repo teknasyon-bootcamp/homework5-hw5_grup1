755a06fd3a287ba4a6044f3f4ed43f44a737254a
BasriMaster <hasannolur06@gmail.com>
2021-09-15 18:41:14 +0300

    index.php template bitiş, config dosya yapısı ekleme

---
+++ views/index.php
@@ -0,0 +1,269 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+    <title>Grup 3 Kitap Uygulaması</title>
+
+
+    <script src="https://kit.fontawesome.com/c400c00162.js" crossorigin="anonymous"></script>
+
+    <style>
+        .baslik{
+            border-left: 3px solid orange;
+        }
+    </style>
+
+  </head>
+  <body>
+    
+
+
+        <!-- navbar ksmı başlangıç -->
+
+    <nav class="navbar navbar-expand-lg navbar-light ps-5">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Grup1</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="./index.php">Anasayfa <i class="fas fa-home"></i></a>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </nav>
+
+        <!-- navbar ksmı bitiş -->
+
+        <!-- slider kısmı başlangıç -->
+
+    <div class="container-fluid">
+
+        <div class="row">
+            <div class="col-md-8 p-1">
+                <img src="../config/images/asf.jpg" style="height:400px" class="img-fluid" alt="">
+            </div>
+            <div class="col-md-4 p-0 slider-height">
+                <div class="col-12 p-1" >
+                    <img src="../config/images/asf.jpg" class="img-fluid" style="height:196px" alt="">
+                </div>
+                <div class="col-12 p-1">
+                    <img src="../config/images/asf.jpg" class="img-fluid" style="height:196px" alt="">
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+        <!-- slider kısmı bitiş -->
+
+        <!-- ürünler kısmı başlangıç -->
+
+    <div class="container mt-4">
+        <div class="row">
+            <h4 class="text-warning baslik">Ürünlerimiz <i class="fas fa-book"></i></h4>
+        </div>
+
+        <!-- ürün listesi başlangıç -->
+        <!-- döngüye girecek kısım -->
+
+        <div class="row mt-3">
+
+            <div class="col p-0 d-flex justify-content-center text-center">
+                <div class="card" style="width: 17rem;">
+                <img src="../config/images/kitap.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Dede Korkut</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-outline-primary">Detay</a>
+                </div>
+                </div>
+            </div>
+            <div class="col p-0 d-flex justify-content-center text-center">
+                <div class="card" style="width: 17rem;">
+                <img src="../config/images/kitap2.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Öylesine Bir Kitap</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-outline-primary">Detay</a>
+                </div>
+                </div>
+            </div>
+            <div class="col p-0 d-flex justify-content-center text-center">
+                <div class="card" style="width: 17rem;">
+                <img src="../config/images/kitap3.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"> Rüya Yorumları</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-outline-primary">Detay</a>
+                </div>
+                </div>
+            </div>
+            <div class="col p-0 d-flex justify-content-center text-center">
+                <div class="card" style="width: 17rem;">
+                <img src="../config/images/kitap4.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">İnsan İklimi</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-outline-primary">Detay</a>
+                </div>
+                </div>
+            </div>
+
+        </div>
+
+        <!-- döngüye girecek kısım bitiş -->
+
+        <div class="row mt-3">
+
+            <div class="col p-0 d-flex justify-content-center text-center">
+                <div class="card" style="width: 17rem;">
+                <img src="../config/images/kitap.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Dede Korkut</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-outline-primary">Detay</a>
+                </div>
+                </div>
+            </div>
+            <div class="col p-0 d-flex justify-content-center text-center">
+                <div class="card" style="width: 17rem;">
+                <img src="../config/images/kitap2.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Öylesine Bir Kitap</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-outline-primary">Detay</a>
+                </div>
+                </div>
+            </div>
+            <div class="col p-0 d-flex justify-content-center text-center">
+                <div class="card" style="width: 17rem;">
+                <img src="../config/images/kitap3.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"> Rüya Yorumları</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-outline-primary">Detay</a>
+                </div>
+                </div>
+            </div>
+            <div class="col p-0 d-flex justify-content-center text-center">
+                <div class="card" style="width: 17rem;">
+                <img src="../config/images/kitap4.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">İnsan İklimi</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-outline-primary">Detay</a>
+                </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row mt-3">
+
+            <div class="col p-0 d-flex justify-content-center text-center">
+                <div class="card" style="width: 17rem;">
+                <img src="../config/images/kitap.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Dede Korkut</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-outline-primary">Detay</a>
+                </div>
+                </div>
+            </div>
+            <div class="col p-0 d-flex justify-content-center text-center">
+                <div class="card" style="width: 17rem;">
+                <img src="../config/images/kitap2.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Öylesine Bir Kitap</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-outline-primary">Detay</a>
+                </div>
+                </div>
+            </div>
+            <div class="col p-0 d-flex justify-content-center text-center">
+                <div class="card" style="width: 17rem;">
+                <img src="../config/images/kitap3.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"> Rüya Yorumları</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-outline-primary">Detay</a>
+                </div>
+                </div>
+            </div>
+            <div class="col p-0 d-flex justify-content-center text-center">
+                <div class="card" style="width: 17rem;">
+                <img src="../config/images/kitap4.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">İnsan İklimi</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-outline-primary">Detay</a>
+                </div>
+                </div>
+            </div>
+
+            </div>
+
+        <!-- ürün listesi bitiş -->
+
+    </div>
+
+        <!-- ürünler kısmı bitiş -->
+
+
+        <!-- pagination (sayfalama) başlangıç -->
+
+        <div class="container mt-3 d-flex justify-content-center">
+            <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li class="page-item">
+                <a class="page-link" href="#" aria-label="Previous">
+                    <span aria-hidden="true">&laquo;</span>
+                </a>
+                </li>
+                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                <li class="page-item"><a class="page-link text-danger" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item">
+                <a class="page-link" href="#" aria-label="Next">
+                    <span aria-hidden="true">&raquo;</span>
+                </a>
+                </li>
+            </ul>
+            </nav>
+        </div>
+        
+        
+        
+        
+        <!-- pagination (sayfalama) bitiş -->
+        
+
+        <!-- footer başlangıç -->
+        <div class="container-fluid text-center text-muted mt-3 p-3 border-top">
+            Powered By Grup_1 <i class="far fa-copyright"></i>
+        </div>
+        <!-- footer bitiş -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+  </body>
+</html>
