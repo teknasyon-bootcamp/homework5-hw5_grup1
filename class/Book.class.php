37f1cf5fb6fe7144e45c214e49278328f7c95fe9
esraakocak <esra1kocak@gmail.com>
2021-09-17 16:39:21 +0300

    update book.class.php

--- class/Book.class.php
+++ class/Book.class.php
@@ -1,80 +1,77 @@
 <?php
 
-class Book{
-	public $db;
-	public function __construct(){
-	$this->db = new database\Database();
-	}
+class Book
+{
+    public $db;
+    public function __construct()
+    {
+        $this->db = new database\Database();
+    }
     //index.php - Kitap listesi ve her kitap için yazar isimleri yer alıyor mu?
-     public function  bookList(){
-		$this->db->create("book",array("name"=>"sam","summary"=>"faasd","author"=>"asdasd","page_count"=>22,"image_url"=>"asdasd"));
-		print_r($this->db->all("book"));
-	
-	}
+    public function  bookList()
+    {
+        //$this->db->create("book",array("name"=>"sam","summary"=>"faasd","author"=>"asdasd","page_count"=>22,"image_url"=>"asdasd"));
+        //  $this->db->setDriver(new database\engine\mysql());
+        return $this->db->all("book");
+    }
     //kitap silme fonksiyonu
     public function deleteBook(int $id)
     {
-       $this->db->delete("book",$id);
-
-       print_r($this->db->all("book"));
-
+        return $this->db->delete("book", $id);
     }
-    
+
     /*// kitap editleme fonksiyonu
-    public function edit_Book(int $id)
+    public function edit_Book(int $id,array $data)
     {
-        $this->db->update("book",$id,array("name"=>"sam","summary"=>"faasd","author"=>"asdasd","page_count"=>22,"image_url"=>"asdasd"));
-        print_r($this->db->all("book"));
-    } */
+       return  $this->db->update("book",$id,array($data);
+       
+    }  */
 
     //kitap güncelleme fonksiyonu 
-    public function updateBook(int $id) // field bir array  içinde book sütünları var
+    public function updateBook(int $id, array $data) // field bir array  içinde book sütünları var
     {
-        $this->db->update("book",$id,array("name"=>"sam","summary"=>"faasd","author"=>"asdasd","page_count"=>22,"image_url"=>"asdasd"));
-        print_r($this->db->all("book"));
 
+        return  $this->db->update("book", $id, array($data));
     }
-     
+
     //kitap ekleme fonksiyonu
-    public function addBook(){   // field bir array  içinde book sütünları var
-        $this->db->create("book",array("name"=>"deneme","summary"=>"faasd","author"=>"deneme","page_count"=>223,"image_url"=>"deneme"));
-        print_r($this->db->all("book"));
-
-
+    public function addBook(array $data)
+    {   // field bir array  içinde book sütünları var
+        return  $this->db->create("book", array($data));
     }
+    
     // kitaba yeni bölüm ekleme fonksiyonu
-    public function sectionAdd(){ // field bir array  içinde sectionun sütünları var
-        $this->db->create("section",array("name"=>"deneme","created_at"=>"faasd","update_at"=>"deneme"));
-        print_r($this->db->all("section"));   
-
+    public function sectionAdd()
+    { // field bir array  içinde sectionun sütünları var
+        $this->db->create("section", array("name" => "deneme", "created_at" => "faasd", "update_at" => "deneme"));
+        print_r($this->db->all("section"));
     }
+
     // kitapta bölüm editleme fonksiyonu 
     public function sectionEdit(int $id)
     {
-     
-        $this->db->update("section",$id,array("name"=>"deneme","created_at"=>"faasd","update_at"=>"deneme"));
-        print_r($this->db->all("section"));   
 
-    } 
+        $this->db->update("section", $id, array("name" => "deneme", "created_at" => "faasd", "update_at" => "deneme"));
+        print_r($this->db->all("section"));
+    }
+
     // kitapta ki bölümleri güncelleme fonksiyonu 
     public function sectionUpdate(array $field, $id) // field bir array  içinde book sütünları var
     {
-        $this->db->update("section",$id,array("name"=>"deneme","created_at"=>"faasd","update_at"=>"deneme"));
-        print_r($this->db->all("section"));   
+       return  $this->db->update("section", $id, array($field));
+   
     }
 
     //kitaptan bölüm silme fonksiyonu
     public function deleteSection(int $id)
     {
-      $this->db->delete("section",$id);
-      print_r($this->db->all("section"));  
+        return $this->db->delete("section", $id);
     }
 
     //kitaptaki bölümü var ise listele 
-    public function sectionList()
+    /*public function sectionList()
     {
       $this->db->sectionList();
       print_r($this->db->all("book"));  
-    }
-
+    }*/
 }
