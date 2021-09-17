6c2d2cb3739ebf3dc00d9bb04d21b0937c6a84c6
Hasan UYANIK <40677383+hasanuyanik@users.noreply.github.com>
2021-09-17 03:56:35 +0300

    Update DriverI.class.php
    Fonksiyonların static'e dönüştürülmesi

--- class/database/engine/DriverI.class.php
+++ class/database/engine/DriverI.class.php
@@ -2,9 +2,9 @@
 namespace database\engine; 
 interface DriverI
 {
-    public function all(String $table):array;
-    public function find(String $table,mixed $id):mixed;
-    public function create(String $table,array $values):bool;
-    public function update(String $table,mixed $id,array $values):bool;
-    public function delete(String $table,mixed $id):bool;
+    public static function all(String $table):array;
+    public static function find(String $table,mixed $id):mixed;
+    public static function create(String $table,array $values):bool;
+    public static function update(String $table,mixed $id,array $values):bool;
+    public static function delete(String $table,mixed $id):bool;
 }
