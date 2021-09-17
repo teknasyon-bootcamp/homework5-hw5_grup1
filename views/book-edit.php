e78bfda01a02e45e7184d152a05e773441119478
Hasan UYANIK <40677383+hasanuyanik@users.noreply.github.com>
2021-09-17 17:30:02 +0300

    Revert "Merge branch 'main' of https://github.com/teknasyon-bootcamp/homework5-hw5_grup1 into main"
    This reverts commit cd901052222454f665e6dbebdba744d1b0a7073e, reversing
    changes made to 3a9eccbc9864c52fa13e74f0eb06ed429197decc.

--- views/book-edit.php
+++ views/book-edit.php
@@ -1,7 +1,13 @@
 <?php
 
-
-echo "kitap editleme ve guncelleme formu";
+if(isset($_GET['id']))
+{
+    echo "kitap editleme ve guncelleme formu";
+    $deneme =new Book;
+    $edit-> editBook($_GET['id']);
+    $update->updateBook($_GET['id'],$field);
+ 
+}
 
 
 //editBook id ile gidicek update ile edit aynı şey return true sa kitap guncellendi & editlendşi ?
