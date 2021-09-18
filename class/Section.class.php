054757d611ceaaa295717b9646ee04fd12ba63e7
designali32 <alicengelcik@gmail.com>
2021-09-18 13:12:09 +0300

    Section class SectionFind ve BookFind eklendi

--- class/Section.class.php
+++ class/Section.class.php
@@ -7,7 +7,14 @@ class Section
     {
         $this->db = new database\Database();
     }
-
+    public function  BookFind(int $id)
+    {
+        return $this->db->find("book",$id);
+    }
+    public function  SectionFind(int $id)
+    {
+        return $this->db->find("section",$id);
+    }
     public function  sectionList()
     {
         return $this->db->all("section");
@@ -20,11 +27,11 @@ class Section
 
     public function updateSection(int $id, array $data)
     {
-        return  $this->db->update("section", $id, array($data));
+        return  $this->db->update("section", $id, $data);
     }
 
     public function addSection(array $data)
     {
-        return  $this->db->create("section", array($data));
+        return  $this->db->create("section", $data);
     }
 }
