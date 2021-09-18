9a43b9909b3c5010037ae780188e7a4ece55701d
esraakocak <esra1kocak@gmail.com>
2021-09-18 12:52:43 +0300

    update

--- views/book-delete.php
+++ views/book-delete.php
@@ -8,7 +8,6 @@ if($_GET['id'])
  
     Header('Location: book-store.php');
 }
-echo "delete işlemi yapıldığı fonksıyon";
 
 
-// deleteBook fonksıyonuna id ile gidip silme işlemi yapılıyor return == true ise evet silindi.
+
