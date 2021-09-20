5191deb0a5083c783dfb5b46284995edbb6fd123
Hasan UYANIK <40677383+hasanuyanik@users.noreply.github.com>
2021-09-20 18:05:35 +0300

    Yorum satırlarının eklenmesi Not: Hasan UYANIK tarafı tamamlandı.. :)

--- class/logger/logger.class.php
+++ class/logger/logger.class.php
@@ -1,55 +1,57 @@
 <?php
-namespace logger;
+namespace logger; // logger namespace tanımlaması
 
-use database\Database;
+use database\Database; // database\Database 'ı kullan
 use logger\driver\{
-    DatabaseLog,
-    FileLog,
-    LogDriverI
+    DatabaseLog,  // logger\driver\DatabaseLog 'u kullan
+    FileLog, // logger\driver\FileLog 'u kullan
+    LogDriverI // logger\driver\LogDriverI 'ı kullan
 };
-use logger\loggableI;
+use logger\loggableI; // logger\loggableI 'ı kullan
 
-class logger implements loggableI
+class logger implements loggableI // logger sınıfı oluştur ve loggableI'ı implement'e et
 {
-    protected LogDriverI $logdriver;
+    protected LogDriverI $logdriver; // protected olarak LogDriverI tipinde logdriver property'sini tanımla
 
     public function __construct()
     {
+        $logging = "null"; // null değerini logging'e ata
         // Config
-        $configdir = (__DIR__)."/../../config.php";
-        if(file_exists($configdir)){
-            $this->config = require $configdir;
-            $this->logging= $this->config["logging"];
+        $configdir = (__DIR__)."/../../config.php"; // config bilgilerinin bulunduğu dosyayı configdir'e ata
+        if (file_exists($configdir)) // configdir içerisinde tanımlı dosya mevcutsa
+        {
+            $this->config = require $configdir; // configdir içerisindeki dosyada verilen değerleri config property'sine ata
+            $logging= $this->config["logging"]; //config logging değerini engine property'sine ata
         }
 
-        if ($this->logging == "database")
+        if ($logging == "database") // logging değeri database ise
         {
-            $this->logdriver = new DatabaseLog(new Database());
+            $this->logdriver = new DatabaseLog(new Database()); // yeni database nesnesi ile DatabaseLog nesnesini oluşturup logdriver property'sine ata
         }
-        elseif ($this->logging == "file")
+        elseif ($logging == "file") // logging değeri file ise
         {
-            $this->logdriver = new FileLog();
+            $this->logdriver = new FileLog(); // FileLog nesnesi oluşturup logdriver property'sine ata
         }
-        elseif ($this->logging == "null")
+        elseif ($logging == "null") // logging değeri null ise
         {
-            return;
+            return; // varsayılan değer döndür
         }
 
-        $this->setDriver($this->logdriver);
+        $this->setDriver($this->logdriver); // güncel logdriver property değerini setDriver fonksiyonunda kullan
     }
 
-    public function setDriver(LogDriverI $logdriver):void
+    public function setDriver(LogDriverI $logdriver):void // setDriver olarak logdriver değerini alacağını ve void değeri ile dönüş yapacağını belirt
     {
-        $this->logdriver = $logdriver;
+        $this->logdriver = $logdriver; // alınan logdriver değerini logdriver property'sine ata
     }
 
-    public static function log(string $message, int $level):void
+    public static function log(string $message, int $level):void // static log olarak message, level değerlerini alacağını ve void ile dönüş yapacağını belirt
     {
-        $log = new static();
+        $log = new static(); // logger class tipinde nesneyi oluşturup log'a ata
 
-        if (isset($log->logdriver))
+        if (isset($log->logdriver)) // log nesnesinin içinde logdriver varsa
         {
-            $log->logdriver->log($message, $level);
+            $log->logdriver->log($message, $level); // log nesnesindeki ilgili driverdaki log fonksiyonunu message, level değerleriyle çalıştır
         }
     }
 }
